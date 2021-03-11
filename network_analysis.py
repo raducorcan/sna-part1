1bb7946f5e07be0b03ee06dd2145b35352a27d38
raducorcan <corcanradu@gmail.com>
2021-03-11 20:06:38 +0200

    network stats analysis

--- network_analysis.py
+++ network_analysis.py
@@ -3,10 +3,9 @@ import collections
 import matplotlib.pyplot as plt
 from pyvis.network import Network
 
-
-# def save_sub_graph(graph):
-#     graph = nx.subgraph(graph, list(range(1, 1001)))
-#     nx.write_gml(graph, "./res/netscience.gml")
+'''
+Uncomment lines 
+'''
 
 def counter_scatter_plot(values, *, title="Title", plot_type=plt.loglog):
     value_counts = collections.Counter(values)
@@ -28,7 +27,6 @@ def plot_distributions(graph):
     counter_scatter_plot(betwn_centr, title="Betweenness centrality distribution")
 
     comp_lens = [len(comp) for comp in nx.connected_components(graph)]
-    print(max(nx.connected_components(graph)))
     counter_scatter_plot(comp_lens, title="Connected components size distribution")
 
 
@@ -42,25 +40,34 @@ def print_stats(graph):
     print(f"Max degree: {max(degs)}")
     print(f"Average degree: {sum(degs) / len(degs)}")
     print(f"Average clustering coefficient: {nx.average_clustering(graph)}")
-    # print(f"Diameter: {nx.diameter(graph)}")
 
     print(f"Graph has {len(list(nx.connected_components(graph)))} connected component(s)")
+    print(f"The largest component has {len(max(nx.connected_components(graph), key=len))} nodes")
+    print(f"Its diameter is {nx.diameter(graph.subgraph(max(nx.connected_components(graph), key=len)))}")
     plot_distributions(graph)
     print("------------------------------------------")
 
 
+def do_analysis(graph):
+    print_stats(graph)
+
+    # network visualisation
+
+    # net = Network(height=720, width=1400)
+    # net.from_nx(graph)
+    # net.show("netscience.html")
+
 def run():
     graph = nx.read_gml("./res/netscience.gml", label="id")
     nodes = len(nx.nodes(graph))
-    # er_graph = nx.fast_gnp_random_graph(nodes, 0.0025)
-    # ba_graph = nx.barabasi_albert_graph(nodes, 2)
+
+    er_graph = nx.fast_gnp_random_graph(nodes, 0.0025)  # random graph using Erdos-Renyi model
+    ba_graph = nx.barabasi_albert_graph(nodes, 2)  # random graph using Barabasi-Albert model
 
     # print_stats(graph)
     # print_stats(er_graph)
     # print_stats(ba_graph)
 
-    net = Network(height=720, width=1400)
-    net.from_nx(graph)
-    net.show("netscience.html")
+
 
 run()
